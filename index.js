cdd2e8a809efe61c8220b94eaa03a17142d0fddc
huykai <hulrui@163.com>
2017-05-02 01:52:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// This tool will run as faulty scene handler
+// 1, issue when kpi lower to threshold
+// 2, perform  log collect
+//  2.1 phase diff with mme, saegw, ce, fw, cg, traffica
+//  2.2 each phase have many task , include many command
+//  2.3 textfsm filter the log, get key metric table
+// 3, perform report
